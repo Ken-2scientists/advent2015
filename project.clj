90f61f98e972924747ffefcf98fe30ead75d232e
Ken Smith <ken.smith@2scientists.net>
2021-01-23 14:10:18 -0700

    Day 12 solns parts 1 and 2

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/math.combinatorics "0.1.6"]
+                 [cheshire "5.10.0"]
                  [advent-utils "0.1.1"]]
   :repl-options {:init-ns advent2015.core})
