ea0a1f71389ce534a8d9f6176eea363ff82faaaa
Ken Smith <ken.smith@2scientists.net>
2021-02-13 16:39:37 -0700

    Day 24 parts 1 and 2

--- src/advent2015/day24.clj
+++ src/advent2015/day24.clj
@@ -4,37 +4,32 @@
 
 (def day24-input (map read-string (u/puzzle-input "day24-input.txt")))
 
-(def day24-sample (concat (range 1 6) (range 7 12)))
+(defn smallest-groups
+  [groups input]
+  (let [target (/ (reduce + input) groups)]
+    (loop [size 1 partitions []]
+      (if (seq partitions)
+        partitions
+        (recur (inc size) (filter #(= target (reduce + %)) (combo/combinations input size)))))))
 
-(defn partition-weight
+(defn best-quantum-entanglement
+  [partitions]
+  (apply min (map (partial reduce *) partitions)))
+
+(defn best-qe-thirds
   [input]
-  (/ (reduce + input) 3))
+  (->> (smallest-groups 3 input)
+       best-quantum-entanglement))
 
-(defn valid-partition?
-  [weight parts]
-  (every? #(= weight %) (map (partial reduce +) parts)))
-
-(defn valid-partitions
+(defn best-qe-fourths
   [input]
-  (let [weight (partition-weight input)]
-    (filter (partial valid-partition? weight) (combo/partitions input :min 3 :max 3))))
-
-(defn best-partitions
-  [input]
-  (let [partitions (valid-partitions input)
-        min-size (apply min (map #(apply min (map count %)) partitions))]
-    (filter #(= min-size (apply min (map count %))) partitions)))
-
-(defn quantum-entanglement
-  [partition]
-  (let [group1 (apply min-key count partition)]
-    (reduce * group1)))
-
-(defn ideal-quantum-entaglement
-  [input]
-  (let [partitions (best-partitions input)]
-    (apply min (map quantum-entanglement partitions))))
+  (->> (smallest-groups 4 input)
+       best-quantum-entanglement))
 
 (defn day24-part1-soln
   []
-  (ideal-quantum-entaglement day24-input))
+  (best-qe-thirds day24-input))
+
+(defn day24-part2-soln
+  []
+  (best-qe-fourths day24-input))
